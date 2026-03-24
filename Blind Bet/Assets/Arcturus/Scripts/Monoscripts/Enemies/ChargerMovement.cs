af72f089ca9315f4249b5da2286a8d01faa481a7
Arcturus <systriekglitchyt@gmail.com>
2026-03-24 09:39:06 -0700

    first area enemies done

---
+++ Blind Bet/Assets/Arcturus/Scripts/Monoscripts/Enemies/ChargerMovement.cs
@@ -0,0 +1,123 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+
+public class ChargerMovement : EnemyMovement
+{
+    public float chargeAcceleration;
+    public float chargeTime;
+    protected override void Start()
+    {
+        rb2d.linearDamping = friction;
+        enemy = new Enemy(10, 20, 5, 2, 6);
+        currentState = StateMachine.patrol;
+    }
+    protected override void Update()
+    {
+        switch (currentState)
+        {
+            case StateMachine.patrol:
+                Patrol();
+                break;
+            case StateMachine.engage:
+                Engage();
+                break;
+        }
+        if (enemyTarget == null && currentState != StateMachine.patrol)
+        {
+            currentState = StateMachine.patrol;
+            path.Clear();
+        }
+        else if (enemyTarget != null && currentState != StateMachine.engage)
+        {
+            currentState = StateMachine.engage;
+            path.Clear();
+        }
+        CreatePath();
+        movedNode = AStarManager.instance.FindNearestNode(transform.position);
+    }
+    protected override void FixedUpdate()
+    {
+        if (enemyTarget != null)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, TargetDirection(enemyTarget.transform.position), 3, hitLayer);
+            Debug.DrawRay(rb2d.position, TargetDirection(enemyTarget.transform.position) * 3f, Color.red);
+            if (hasKnockback)
+                return;
+            if (isAttacking)
+            {
+                rb2d.linearDamping = 0;
+                Vector2 newVelocity = TargetDirection(movementTarget.position) * chargeAcceleration;
+                rb2d.AddForce(newVelocity);
+                Vector2 velocity = Vector2.ClampMagnitude(new(rb2d.linearVelocity.x, rb2d.linearVelocity.y), enemy.topSpeed);
+                rb2d.linearVelocity = velocity;
+                return;
+            }
+            distance = TargetDistance(enemyTarget.transform.position);
+            if (distance > stopRange && hit)
+            {
+                rb2d.linearDamping = 0;
+                Vector2 newVelocity = TargetDirection(movementTarget.position) * acceleration;
+                rb2d.AddForce(newVelocity);
+                Vector2 velocity = Vector2.ClampMagnitude(new(rb2d.linearVelocity.x, rb2d.linearVelocity.y), enemy.topSpeed);
+                rb2d.linearVelocity = velocity;
+            }
+            if (distance > stopRange && !hit && path.Count > 0)
+            {
+                rb2d.linearDamping = 0;
+                Vector2 newVelocity = TargetDirection(new Vector2(path[0].transform.position.x, path[0].transform.position.y)) * acceleration;
+                rb2d.AddForce(newVelocity);
+                Vector2 velocity = Vector2.ClampMagnitude(new(rb2d.linearVelocity.x, rb2d.linearVelocity.y), enemy.topSpeed);
+                rb2d.linearVelocity = velocity;
+            }
+            if (distance < AttackRange && canAttack)
+            {
+                StartCoroutine(AttackTimer());
+            }
+            if (distance < stopRange && !isAttacking)
+            {
+                rb2d.linearDamping = friction;
+            }
+            if (distance > 40)
+            {
+                enemyTarget = null;
+                rb2d.linearDamping = friction;
+            }
+        }
+        if (enemyTarget == null)
+        {
+            if (path.Count > 0)
+            {
+                rb2d.linearDamping = 0;
+                Vector2 newVelocity = TargetDirection(new Vector2(path[0].transform.position.x, path[0].transform.position.y)) * acceleration;
+                rb2d.AddForce(newVelocity);
+                Vector2 velocity = Vector2.ClampMagnitude(new(rb2d.linearVelocity.x, rb2d.linearVelocity.y), enemy.topSpeed);
+                rb2d.linearVelocity = velocity;
+            }
+        }
+    }
+    protected override IEnumerator AttackTimer()
+    {
+        canAttack = false; // make the enemy not duplicate attacks
+        isReadyingAttack = true; // small moment before attack to make it not instant
+        spriteRend.color = new Color32(230, 80, 180, 255);
+        yield return new WaitForSeconds(0.5f); // amount of time to react to attack
+        isReadyingAttack = false; // no longer readying attack
+        spriteRend.color = new Color32(200, 200, 200, 255);
+        isAttacking = true; // is now attacking
+        enemy.topSpeed = 6;
+        yield return new WaitForSeconds(chargeTime); // time where you can take damage/parry/get shot at
+        isAttacking = false; // no longer attacking
+        enemy.topSpeed = 2;
+        yield return new WaitForSeconds(enemy.attackCooldown); // cooldown so the enemies don't spam attacks
+        canAttack = true; // can attack again
+    }
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && isAttacking)
+        {
+            PlayerMovement pm = collision.GetComponent<PlayerMovement>();
+            pm.GetHit(this, enemy.baseKnockback);
+        }
+    }
+}
