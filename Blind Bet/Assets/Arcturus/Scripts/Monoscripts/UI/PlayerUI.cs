d238b020297ace27d2e34f8915245d75e4fa9ecd
Arcturus <systriekglitchyt@gmail.com>
2026-04-21 04:55:49 -0700

    chip bar now has changing chip

--- Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/PlayerUI.cs
+++ Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/PlayerUI.cs
@@ -13,6 +13,8 @@ public class PlayerUI : MonoBehaviour
     [Header("Chips")]
     public Slider chipbarSlider;
     public TMP_Text chipbarText;
+    public Image[] chips = new Image[6];
+    public Image currentChip;
 
 
     private void Update()
@@ -27,8 +29,25 @@ public class PlayerUI : MonoBehaviour
             healthbarText.SetText("{0}"+"/"+"{1}",pm.playerStats.currentHealth,pm.playerStats.maxHealth);
         // chips
         if(chipbarSlider.value != pm.playerStats.currentChips/pm.playerStats.maxChips)
-            chipbarSlider.value = Mathf.MoveTowards(chipbarSlider.value, pm.playerStats.currentChips/pm.playerStats.maxChips, 1f * Time.deltaTime);;
+            chipbarSlider.value = Mathf.MoveTowards(chipbarSlider.value, pm.playerStats.currentChips/pm.playerStats.maxChips, 1f * Time.deltaTime);
         if(chipbarText.text != ""+pm.playerStats.currentChips)
             chipbarText.SetText("{0}",pm.playerStats.currentChips);
+        if(chipbarSlider.value == 1 && chips[5].sprite != currentChip.sprite)
+            SwitchChipPicture(5);
+        else if(chipbarSlider.value >= 0.8f && chipbarSlider.value < 1 && chips[4].sprite != currentChip.sprite)
+            SwitchChipPicture(4);
+        else if(chipbarSlider.value >= 0.6f && chipbarSlider.value < 0.8f && chips[3].sprite != currentChip.sprite)
+            SwitchChipPicture(3);
+        else if(chipbarSlider.value >= 0.4f && chipbarSlider.value < 0.6f && chips[2].sprite != currentChip.sprite)
+            SwitchChipPicture(2);
+        else if(chipbarSlider.value > 0.1f && chipbarSlider.value < 0.4f && chips[1].sprite != currentChip.sprite)
+            SwitchChipPicture(1);
+        else if(chipbarSlider.value >= 0 && chipbarSlider.value <= 0.1f && chips[0].sprite != currentChip.sprite)
+            SwitchChipPicture(0);
+    }
+
+    private void SwitchChipPicture(int chipIndex)
+    {
+        currentChip.sprite = chips[chipIndex].sprite;
     }
 }
