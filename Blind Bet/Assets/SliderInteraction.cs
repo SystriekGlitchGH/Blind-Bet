6d1b01b51ef5e7cdf8832960bc84e8070826626d
MUKILTEO\1725232 <1725232@wednet.mukilteo.edu>
2026-04-23 10:22:57 -0700

    working on volume slider thing still

---
+++ Blind Bet/Assets/SliderInteraction.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SliderInteraction : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TMP_Text textField;
+    [SerializeField] private bool showDecimalPoints;
+
+
+    private void Reset()
+    {
+        slider = GetComponent<Slider>();
+        textField = GetComponentInChildren<TMP_Text>();
+    }
+
+    public void HandleSliderValueChanged(float value)
+    {
+        if (showDecimalPoints)
+            textField.SetText(value.ToString(format:"F2"));
+        else
+            textField.SetText(value.ToString(format:"F0"));
+    }
+}
