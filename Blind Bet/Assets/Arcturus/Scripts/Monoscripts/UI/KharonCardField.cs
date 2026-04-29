6695410d0a83641c797c522ec3b58c6fa982e472
Arcturus <systriekglitchyt@gmail.com>
2026-04-29 09:58:14 -0700

    saved script

--- Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/KharonCardField.cs
+++ Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/KharonCardField.cs
@@ -6,6 +6,9 @@ public class KharonCardField : MonoBehaviour
     Random rand = new Random();
     private void Awake()
     {
-        
+        for(int i = 0; i < 3; i++)
+        {
+
+        }
     }
 }
