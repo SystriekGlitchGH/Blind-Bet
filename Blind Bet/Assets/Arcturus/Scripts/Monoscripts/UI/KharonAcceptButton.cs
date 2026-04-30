87b668b1f6d327e24639ee1528030a710fed28a3
Arcturus <systriekglitchyt@gmail.com>
2026-04-30 09:17:49 -0700

    almost finished with kharon picker

--- Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/KharonAcceptButton.cs
+++ Blind Bet/Assets/Arcturus/Scripts/Monoscripts/UI/KharonAcceptButton.cs
@@ -1,14 +1,23 @@
 using TMPro;
 using UnityEngine;
+using static Card;
 
 public class KharonAcceptButton : MonoBehaviour
 {
     public GameObject cardField;
     public TMP_Text buff;
     public TMP_Text debuff;
-    void Awake()
+    public void AcceptHand()
     {
+        Debug.Log("Button pressed");
         PlayerMovement pm = FindFirstObjectByType<PlayerMovement>();
-        foreach(Transform child in cardField.transform)
+        for(int i = 0; i < 3; i++)
+        {
+            cardField.transform.GetChild(i).GetComponent<DraggableItem>().enabled = true;
+            pm.playerStats.bench.Add(cardField.transform.GetChild(i).GetComponent<DraggableItem>().card);
+            pm.playerStats.buffDebuffs.Add(buff.text);
+            pm.playerStats.buffDebuffs.Add(debuff.text);
+            Instantiate(cardField.transform.GetChild(i).gameObject,pm.playerUI.bench);
+        }
     }
 }
